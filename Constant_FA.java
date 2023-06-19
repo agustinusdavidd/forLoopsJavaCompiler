a58f0752263e0f6c036e8488d586ccd4e02c03aa
CraftTex <nadinezuhdan@gmail.com>
2023-06-19 14:18:18 +0700

    Constant can now detect negative integers

--- Constant_FA.java
+++ Constant_FA.java
@@ -5,21 +5,24 @@ public class Constant_FA {
 
         String state = "q1";
         String[] baris = new String[] {"q1", "q2", "q3"};
-        String[] kolom = new String[] {"number", "selain number", "EOS"};
+        String[] kolom = new String[] {"number", "-", "selain number", "EOS"};
 
-        String[][] transitionTable = new String[3][3];
+        String[][] transitionTable = new String[3][4];
 
         transitionTable[0][0] = "q2";
-        transitionTable[0][1] = "q3";
-        transitionTable[0][2] = "err";
+        transitionTable[0][1] = "q2";
+        transitionTable[0][2] = "q3";
+        transitionTable[0][3] = "err";
 
         transitionTable[1][0] = "q2";
         transitionTable[1][1] = "q3";
-        transitionTable[1][2] = "acc";
+        transitionTable[1][2] = "q3";
+        transitionTable[1][3] = "acc";
 
         transitionTable[2][0] = "q3";
         transitionTable[2][1] = "q3";
-        transitionTable[2][2] = "err";
+        transitionTable[2][2] = "q3";
+        transitionTable[2][3] = "err";
 
         char symbol;
         String input;
@@ -31,6 +34,8 @@ public class Constant_FA {
             symbol == '4' || symbol == '5' || symbol == '6' || symbol == '7' ||
             symbol == '8' || symbol == '9'){
                 input = "number";
+            } else if (symbol == '-') {
+                input = "-";
             } else if (symbol == '@') {
                 input = "EOS";
             } else if(!(symbol == '0' || symbol == '1' || symbol == '2' || symbol == '3' || 
@@ -63,8 +68,8 @@ public class Constant_FA {
 
     public static void main(String[] args) {
         System.out.println(valid_LB("12312312"));                                   //return true
-        System.out.println(valid_LB("0lk"));                                        //return false
-        System.out.println(valid_LB("lk0"));                                        //return false
-        System.out.println(valid_LB("l0"));                                         //return false
+        System.out.println(valid_LB("-0k"));                                        //return false
+        System.out.println(valid_LB("l-0"));                                        //return false
+        System.out.println(valid_LB("-0"));                                         //return true
     }
 }
